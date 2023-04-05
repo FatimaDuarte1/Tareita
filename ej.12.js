633c9a2758e006bbcfadd7d779b57fc1faf2457b
FatimaDuarte1 <125383851+FatimaDuarte1@users.noreply.github.com>
2023-04-04 21:55:48 -0600

    12.- Hacer un programa que imprima los números del 1 al 100 y que calcule la suma de todos los números pares por un lado, y por otro, la de todos los impares.

---
+++ ej.12.js
@@ -0,0 +1,14 @@
+let sumadePares = 0;
+let sumadeImpares = 0;
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  if (i % 2 === 0) {
+    sumadePares += i;
+  } else {
+    sumadeImpares += i;
+  }
+}
+
+
+
