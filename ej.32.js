0df1d3cb7cd93c17678b3630b91b27feded43283
FatimaDuarte1 <125383851+FatimaDuarte1@users.noreply.github.com>
2023-04-19 23:26:59 -0600

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ ej.32.js
@@ -0,0 +1,8 @@
+const Nombres = ["Isela", "Rene", "Renee", "Alfredo", "Nicole", 
+"Lucia", "Alejandra", "Patricia", "Karen", "Eva", "Luis", "Anna", 
+"Liam", "Jacobo", "Diego", "America", "Marcos", 
+"Jose", "Karla", "Karina"];
+
+for (let i = 0; i < Nombres.length; i++) {
+  console.log(Nombres[i]);
+}
